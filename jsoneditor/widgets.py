6237dd9e5f117e6e8cce3b9d50438242046291d0
Matthias Kestenholz <mk@feinheit.ch>
2018-12-14 19:50:06 +0100

    Future-proof the widget render method

--- jsoneditor/widgets.py
+++ jsoneditor/widgets.py
@@ -32,8 +32,10 @@ class JSONEditorWidget(forms.widgets.Textarea):
             'jsoneditor/js/jsoneditor_widget.js'
         )
 
-    def render(self, name, value, attrs=None):
-        input_field = super(JSONEditorWidget, self).render(name, value, attrs)
+    def render(self, name, value, attrs=None, **kwargs):
+        input_field = super(JSONEditorWidget, self).render(
+            name, value, attrs, **kwargs
+        )
 
         return format_html(
             """
@@ -48,10 +50,9 @@ class JSONEditorWidget(forms.widgets.Textarea):
                 </div>
             </div>
             """,
-            **{
-                'field': input_field,
-                'config': json.dumps(self.config),
-            })
+            field=input_field,
+            config=json.dumps(self.config),
+        )
 
 
 class FeinCMSJSONEditorWidget(JSONEditorWidget):
