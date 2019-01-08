a3a0c19f6a213b532555f8b754df90045a9673d5
Matthias Kestenholz <mk@feinheit.ch>
2019-01-08 15:47:15 +0100

    Update the FeinCMS integration to also work with django-content-editor

--- jsoneditor/static/jsoneditor/js/jsoneditor_feincms_init.js
+++ jsoneditor/static/jsoneditor/js/jsoneditor_feincms_init.js
@@ -1,15 +1,35 @@
-django.jQuery(document).ready(function () {
-    instances = [];
-    contentblock_init_handlers.push(function() {
-        django.jQuery('.order-machine .jsoneditor').each(function() {
-            var editor = init_editor(this);
+django.jQuery(document).ready(function ($) {
+  var instances = [],
+    doUpdate = function() { updateValues(instances); };
 
-            if (editor) {
-                instances.push(editor);
-                editor.on('change', function() {
-                    updateValues(instances);
-                });
-            }
-        });
+
+  if (window.contentblock_init_handlers) {
+    // FeinCMS 1.x
+    contentblock_init_handlers.push(function() {
+      django.jQuery(".order-machine .jsoneditor").each(function() {
+        var editor = init_editor(this);
+
+        if (editor) {
+          instances.push(editor);
+          editor.on("change", doUpdate);
+        }
+      });
     });
+
+  } else {
+    // django-content-editor (obviously!)
+
+    $(document).on("content-editor:activate", function(event, formset) {
+      console.log("Activating", formset);
+      var jsoneditor = formset.find(".jsoneditor");
+      if (jsoneditor.length) {
+        var editor = init_editor(jsoneditor[0]);
+        if (editor) {
+          instances.push(editor);
+          editor.on("change", doUpdate);
+        }
+      }
+    });
+
+  }
 });
